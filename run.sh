c2cff332c981b428d5a77d8fa9eec9335eadb889
Justinas Krencevicius <justinaskrencevicius@lt-justinas.lan>
2023-03-17 23:24:33 +0200

    mount drive if is needed

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
 
 for dir in $dirlist
 do
-    if [$DRIVE != ''] then
+    if [ -z "$DRIVE" ] then
     echo "> mount drive: [$DRIVE] to dir [$dir]"
     mount $DRIVE $dir
     fi
